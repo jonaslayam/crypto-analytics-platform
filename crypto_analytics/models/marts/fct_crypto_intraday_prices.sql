75cb9ce09c95c5b6443749411e59ae22c8406800
Jonas Laya <jonaslaya@workmate.cl>
2026-03-11 13:29:13 -0300

    feat: implement technical indicators

--- crypto_analytics/models/marts/fct_crypto_intraday_prices.sql
+++ crypto_analytics/models/marts/fct_crypto_intraday_prices.sql
@@ -6,10 +6,11 @@
 
 WITH base_prices AS (
     SELECT 
-        STANDARD_HASH(CAST(asset_id AS VARCHAR2(100)) || TO_CHAR(event_time, 'YYYYMMDDHH24MISS'), 'MD5') as fact_sk,
-        CAST(asset_id AS VARCHAR2(100)) as asset_id,
-        CAST(price_usd AS NUMBER(18, 8)) as price_usd,
-        CAST(event_time AS TIMESTAMP) as event_time,
+        price_sk AS fact_sk,
+        asset_id,
+        price_usd,
+        volume_usd_24h,
+        event_time,
         TO_CHAR(event_time, 'YYYYMMDD') as date_key
     FROM {{ ref('stg_crypto_prices') }}
     
@@ -25,6 +26,7 @@ calculated_features AS (
         date_key,
         event_time,
         price_usd,
+        volume_usd_24h,
         
         -- 1. Tendency: Moving Average of 24 hours (6 data points if querying every 4h)
         AVG(price_usd) OVER (
@@ -37,19 +39,47 @@ calculated_features AS (
         LAG(price_usd) OVER (
             PARTITION BY asset_id 
             ORDER BY event_time
-        ) AS last_price_usd
-        
+        ) AS last_price_usd,
+
+        -- 3. Volatility: Standard deviation of price over 24 hours
+        STDDEV(price_usd) OVER (
+            PARTITION BY asset_id 
+            ORDER BY event_time 
+            RANGE INTERVAL '24' HOUR PRECEDING
+        ) AS stddev_24h_usd,
+
+        -- 4. Volume Trend: Volume from the previous call
+        LAG(volume_usd_24h) OVER (
+            PARTITION BY asset_id 
+            ORDER BY event_time
+        ) AS last_volume_usd
+
     FROM base_prices
 ),
 
 final_features AS (
     SELECT 
         cf.*,
+        -- 5. Percentage change since last price
         CASE 
             WHEN cf.last_price_usd IS NOT NULL AND cf.last_price_usd > 0 
             THEN ((cf.price_usd - cf.last_price_usd) / cf.last_price_usd) * 100 
             ELSE 0 
-        END AS pct_change_since_last
+        END AS pct_change_since_last,
+
+        -- 6. Price to Moving Average ratio
+        CASE 
+            WHEN cf.ma_24h_usd > 0 THEN cf.price_usd / cf.ma_24h_usd 
+            ELSE 1 
+        END AS price_to_ma_ratio,
+
+        -- 7. Volume percentage change
+        CASE 
+            WHEN cf.last_volume_usd > 0 
+            THEN ((cf.volume_usd_24h - cf.last_volume_usd) / cf.last_volume_usd) * 100 
+            ELSE 0 
+        END AS volume_pct_change
+
     FROM calculated_features cf
 )
 
@@ -60,7 +90,12 @@ SELECT
     f.event_time,
     f.price_usd,
     f.ma_24h_usd,
-    f.pct_change_since_last
+    f.pct_change_since_last,
+    f.price_to_ma_ratio,
+    f.volume_pct_change,
+    COALESCE(f.stddev_24h_usd, 0) AS stddev_24h_usd,
+    EXTRACT(HOUR FROM f.event_time) AS hour_of_day,
+    TO_CHAR(f.event_time, 'D') AS day_of_week
 FROM final_features f
 LEFT JOIN {{ ref('dim_assets') }} d
     ON f.asset_id = d.asset_id
